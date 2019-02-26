0dea81e1b2f836f331a1653d1434f94ca1b8fff9
Saumen <saumenroy323@gmail.com>
2019-02-26 18:48:01 +0600

    Add files via upload

---
+++ Roman to integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        n = len(s)
+        total = dic[s[n - 1]]
+        for i in range(n - 1, 0, -1):
+            current = dic[s[i]]
+            prev = dic[s[i - 1]]
+            total += prev if prev >= current else -prev
+        return total
