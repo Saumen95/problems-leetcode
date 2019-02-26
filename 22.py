86c717340b2047d3dd06efa8c92839db3dd2368d
Saumen <saumenroy323@gmail.com>
2019-02-26 21:52:51 +0600

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,23 @@
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        result = []
+
+        def backtracking(S, left, right):
+            if len(S) == 2 * n:
+                result.append(S)
+                return
+
+            if left < n:
+                backtracking(S + '(', left + 1, right)
+
+            if right < left:
+                backtracking(S + ')', left, right + 1)
+
+        backtracking('', 0, 0)
+        return result
