a7be86c3f899daf2e790014119674c64108c37ea
Saumen <saumenroy323@gmail.com>
2019-02-28 01:35:07 +0600

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        pre = new_head = ListNode(0)
+        while head and head.next:
+            tmp = head.next
+            head.next = tmp.next
+            tmp.next = head
+            pre.next = tmp
+            pre = head
+            head = head.next
+        return new_head.next
