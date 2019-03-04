1df3f5a4a42f57bfc309bc9bc5063ea655fef1f4
Saumen <saumenroy323@gmail.com>
2019-03-05 00:59:23 +0600

    Add files via upload

---
+++ Majority element.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        temp = dict()
+        if len(nums) == 1:
+            return nums[0]
+        for num in nums:
+            if temp.has_key(num):
+                if temp[num] >= len(nums) / 2:
+                    return num
+                else:
+                    temp[num] += 1
+            else:
+                temp[num] = 1
